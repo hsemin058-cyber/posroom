6b660dcb996f9450429b56ffd2ca8e4e29d5ee0b
hsemin058-cyber <hsemin058@gmail.com>
2026-01-22 18:15:36 +0900

    Create welcomeRender.js

---
+++ welcomeRender.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+  return {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "📦 안녕하세요, 포스룸입니다!\n우편물 수령·보관이 필요한 상황에서 편하게 이용해보세요!"
+          }
+        }
+      ]
+    }
+  };
+};
